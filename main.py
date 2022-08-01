768fc3e6a47de58624727288092a94a976742792
Teff <eowl4588@naver.com>
2022-08-02 00:14:00 +0900

    les plus

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ wb = Workbook(write_only=True)
 ws = wb.create_sheet()
 
 driver = webdriver.Chrome()
-driver.get("https://www.instagram.com/")
+driver.get("https://www.instagram.com/accounts/login/?next=%2Fp%2FCfk_BBKPk1k%2F&source=desktop_nav")
 driver.implicitly_wait(3)
+
 
 # 로그인
 driver.find_element_by_css_selector("#loginForm > div > div:nth-child(1) > div > label > input").send_keys("shxorxxs_")
@@ -22,28 +23,20 @@ time.sleep(1)
 
 # 팝업 종료
 driver.find_element_by_css_selector("#react-root > section > main > div > div > div > div > button").click()
-driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm").click()
 time.sleep(1)
-
-# 계정 접근
-insta_name = driver.find_element_by_css_selector("#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.LWmhU._0aCwM > input")
-insta_name.send_keys('i_am_young22')
-time.sleep(2)
-
-# 첫번째 게시물 클릭
-driver.find_elements_by_css_selector('._9AhH0')[1].click()
-time.sleep(1)
+#driver.find_element_by_class_name('_a9--._a9_1').click()
+#time.sleep(1)
 
 # 댓글 플러스 버튼 누르기
 while True:
     try:
-        button = driver.find_element_by_css_selector('body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.EtaWk > ul > li > div > button > span')
+        button = driver.find_element_by_class_name('_abl-')
     except:
         pass
 
     if button is not None:
         try:
-            driver.find_element_by_css_selector('body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > div.EtaWk > ul > li > div > button > span').click()
+            driver.find_element_by_class_name('_abl-').click()
         except:
             break
 
